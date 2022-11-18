ea678948f4a7ac836094f21ab5581462089c70c4
sahildkun <sahilsahoo23@gmail.com>
2022-11-18 21:41:43 +0530

    home fixed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Artist from '../components/ArtistCard'
+import Footer from '../components/Footer'
+import Hero from '../components/Hero'
+import Middle from '../components/Middle'
 import Navbar from '../components/Navbar'
 
 
@@ -7,6 +11,10 @@ export default function Home() {
   return (
     <>
    <Navbar/>
+   <Hero/>
+   <Middle/>
+   <Artist/>
+   <Footer/>
     </>
   )
 }
